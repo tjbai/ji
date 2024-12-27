646be23bfdd3aa29132c3be6399939f46328238c
TJ Bai <tjianbai@gmail.com>
2024-12-26 23:59:18 -0500

    cleanup

--- src/ji/html.py
+++ src/ji/html.py
@@ -48,15 +48,9 @@ def generate(repo: Repo) -> str:
             --bg-color: #e8e8e8;
             --text-color: #444;
             --dim-color: #444;
-            //--todo-color: #FF6B6B;
-            //--done-color: #98FB98;
-            //--stage-color: #FFD93D;
             --todo-color: #D32F2F;
             --done-color: #2E7D32;
             --stage-color: #F57C00;
-            //--todo-color: #E06C75;
-            //--done-color: #3D9970;
-            //--stage-color: #F4B942;
         }
 
         .page { margin-bottom: 3em; }
@@ -203,7 +197,8 @@ def generate(repo: Repo) -> str:
                         <span class="task-id">{task_id}</span>
                         <span class="task-content {status_class}">{task.content}</span>
                         <span class="timestamp">{format_time(task.last_modified)}</span>
-                    </div>'''
+                    </div>
+                    '''
 
                     if task.comment_list:
                         html_content += '<div class="comments">'
